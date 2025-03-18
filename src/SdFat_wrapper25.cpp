264cfa8b6f0abe244a92858b64eb6e5512bb497d
Woutvstk <119763111+Woutvstk@users.noreply.github.com>
2025-03-18 20:44:00 +0100

    big boy commit 2

--- src/SdFat_wrapper25.cpp
+++ src/SdFat_wrapper25.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 ArduinoOutStream cout(Serial);
 
+
+//########################### SdFat SD card filesystem functions ####################################
+
 bool SdFatWrapper25::begin(uint8_t SdCsPin, SPIClass &spi, uint32_t frequency, const char *mountpoint, uint8_t max_files, bool format_if_empty)
 {
 
@@ -28,43 +31,174 @@ bool SdFatWrapper25::begin(uint8_t SdCsPin, SPIClass &spi, uint32_t frequency, c
 
 uint64_t SdFatWrapper25::cardSize()
 {
-  return (uint64_t)csd.capacity()*512;
+  return (uint64_t)csd.capacity() * 512;
 }
 
 uint64_t SdFatWrapper25::totalBytes()
 {
-  return (uint64_t)sd.sectorsPerCluster()*sd.clusterCount()*512;
+  return (uint64_t)sd.sectorsPerCluster() * sd.clusterCount() * 512;
 }
-
 
 uint64_t SdFatWrapper25::usedBytes()
 {
-  return (uint64_t)sd.sectorsPerCluster()*(sd.clusterCount()-sd.freeClusterCount())*512;
+  return (uint64_t)sd.sectorsPerCluster() * (sd.clusterCount() - sd.freeClusterCount()) * 512;
 }
 
-
 /*
-*Maps SdFat library Sd card types
-*
-* \return 0: No_card/error, 2: SD v1/v2, 3:SDHC
-*/
+ *Maps SdFat library Sd card types to esp32 sd library card types
+ *
+ * \return 0: No_card/error, 2: SD v1/v2, 3:SDHC
+ */
 uint8_t SdFatWrapper25::cardType()
 {
 
-uint8_t returnVal = 0;
+  uint8_t returnVal = 0;
 
-//returns 0 - SD V1, 1 - SD V2, or 3 - SDHC/SDXC
-uint8_t type = sd.card()->type();
+  // returns 0 - SD V1, 1 - SD V2, or 3 - SDHC/SDXC
+  uint8_t type = sd.card()->type();
 
-if(type == 1 || type == 2)
-{
-  returnVal = 2;
+  if (type == 1 || type == 2)
+  {
+    returnVal = 2;
+  }
+  else if (type == 3)
+  {
+    returnVal = 3;
+  }
+
+  return returnVal;
 }
-else if(type == 3)
+
+SdFatFileWrapper25 SdFatWrapper25::open(const String &path, const char *mode, const bool create)
 {
-  returnVal = 3;
+  oflag_t oflag = 0;
+
+  if (strcmp(mode, "r") == 0)
+  {
+    Serial.println("File open mode is Read");
+    oflag = oflag | O_RDONLY;
+  }
+  else if (strcmp(mode, "w") == 0)
+  {
+    Serial.println("File open mode is Write");
+    oflag = oflag | O_TRUNC;  //FIXME, all files will be reset to length 0 when written
+    oflag = oflag | O_WRONLY;
+  }
+  else if (strcmp(mode, "a") == 0)
+  {
+    Serial.println("File open mode is Append");
+    oflag = oflag | O_APPEND;
+  }
+  else
+  {
+    Serial.println("ERROR file open mode invalid");
+  }
+
+  if (create)
+  {
+    Serial.println("File wil be created if not non-existend");
+    oflag = oflag | O_CREAT;
+  }
+
+  SdFatFileWrapper25 file25 = SdFatFileWrapper25();
+
+  if (!file25.init(path, oflag))
+  {
+    Serial.println("File was NOT opened succesfully!!!");
+    errorPrint();
+  }
+  return file25;
 }
 
-return returnVal;
 
+bool SdFatWrapper25::exists(const char *path)
+{
+return sd.exists(path);
+}
+
+
+bool SdFatWrapper25::mkdir(const char *path)
+{
+  return sd.mkdir(path);
+}
+
+bool SdFatWrapper25::remove(const char *path)
+{
+
+return sd.remove(path);
+}
+
+bool SdFatWrapper25::rmdir(const char *path)
+{
+  return sd.rmdir(path);
+}
+
+
+bool SdFatWrapper25::rename(const char *pathFrom, const char *pathTo)
+{
+return sd.rename(pathFrom, pathTo);
+}
+
+
+size_t SdFatFileWrapper25::print(const char *content)
+{
+  return file.print(content);
+}
+
+void SdFatWrapper25::errorPrint()
+{
+  // if (sd.sdErrorCode()) {
+  cout << F("SD errorCode: ") << hex << showbase;
+  printSdErrorSymbol(&Serial, sd.sdErrorCode());
+  cout << F(" = ") << int(sd.sdErrorCode()) << endl;
+  cout << F("SD errorData = ") << int(sd.sdErrorData()) << dec << endl;
+  //}
+}
+
+//########################### SdFat file object functions ####################################
+// init SdFat file object
+// return true if file is opened succesfully
+bool SdFatFileWrapper25::init(const String &path, oflag_t oflag)
+{
+
+  return file.open(path.c_str(), oflag);
+}
+
+size_t SdFatFileWrapper25::write(const uint8_t *buf, size_t size)
+{
+  if (!file.isOpen())
+  {
+    Serial.println("file is not open!");
+  }
+
+  size_t tmpResult = file.write(buf, size);
+  file.sync();
+
+  if (tmpResult != size)
+  {
+    Serial.println("Nr of bytes written does not match string length");
+  }
+  return tmpResult;
+}
+
+size_t SdFatFileWrapper25::read(uint8_t *buf, size_t size)
+{
+  file.rewind();
+  return file.read(buf, size);
+}
+int SdFatFileWrapper25::read()
+{
+  return file.read();
+}
+
+
+
+bool SdFatFileWrapper25::close()
+{
+  return file.close();
+}
+
+int SdFatFileWrapper25::available()
+{
+  return file.available();
 }
--- src/SdFat_wrapper25.h
+++ src/SdFat_wrapper25.h
@@ -8,9 +8,6 @@
 #define SDFAT_FILE_TYPE 1         // only support FAT16/FAT32, not exFAT
 #define CHECK_FLASH_PROGRAMMING 0 // this reduces flash usage but may cause higher power usage when sd card is idle TODO:Check if power usage is higher
 
-#ifdef ENABLE_DEDICATED_SPI
-// cout
-#endif
 
 #include "SdFat/SdFatConfig.h"
 
@@ -23,26 +20,61 @@
 
 #include "sd_defines.h"
 
+class SdFatWrapper25;
+class SdFatFileWrapper25;
+
+// this is the filesystem wrapper class for the underlaying 'SdFat'
 class SdFatWrapper25
 {
 
+
 public:
-//initialise underlaying sd objects, SPI bus must already be initialised
+        // initialise underlaying sd objects, SPI bus must already be initialised
         bool begin(uint8_t ssPin = SS, SPIClass &spi = SPI, uint32_t frequency = 4000000,
                    const char *mountpoint = "/sd", uint8_t max_files = 5, bool format_if_empty = false);
 
-                   //return total card size in bytes
+        // return total card size in bytes
         uint64_t cardSize();
         uint64_t totalBytes();
         uint64_t usedBytes();
         uint8_t cardType();
+        SdFatFileWrapper25 open(const String &path, const char *mode, const bool create);
+        void errorPrint();
+        bool exists(const char *path);
+        bool mkdir(const char *path);
+        bool remove(const char *path);
+        bool rmdir(const char *path);
+        bool rename(const char *pathFrom, const char *pathTo);
+        
 
 private:
         SdFat sd;
-        File file;
         csd_t csd;
 };
 
 
+
+// this is the file wrapper class for the underlaying 'File'
+
+class SdFatFileWrapper25
+{
+public:
+        SdFatFileWrapper25() {}
+        size_t write(const uint8_t *buf, size_t size);
+        size_t read(uint8_t *buf, size_t size);
+        bool close();
+        size_t print(const char *content);
+        int available();
+        int read();
+
+protected:
+        bool init(const String &path, oflag_t oflag);
+
+private:
+        File file;
+
+
+friend class SdFatWrapper25;
+};
 
 #endif
